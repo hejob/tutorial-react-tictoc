65309664fdc3e05f7ed8d5af2f2de6dd54121262
hejob <hejob.moyase@gmail.com>
2019-02-01 19:40:42 +0800

    square interaction

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square"
+                onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
-    renderSquare() {
+    renderSquare(i) {
         return (
-            <Square />
+            <Square value={i} />
         );
     }
 
